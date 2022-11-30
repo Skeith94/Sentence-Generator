94d96ab30a0ed99e86c5186ed262b58abf838b98
Vito Malato <107039302+Skeith94@users.noreply.github.com>
2022-11-30 12:44:52 +0100

    Add files via upload

---
+++ TeamB/sentence/src/main/java/it/cgmconsulting/sentence/service/SubjectService.java
@@ -0,0 +1,23 @@
+package it.cgmconsulting.sentence.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class SubjectService  {
+
+
+    @Autowired
+    WordClientWebClient wordClientWebClient;
+
+
+    public Mono<String> getWord() {
+        return wordClientWebClient.getWord("subject");
+
+    }
+
+
+
+}
