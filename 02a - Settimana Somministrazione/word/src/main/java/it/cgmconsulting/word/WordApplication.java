aec5688128c81662fbd572ebe1bc4f32347a5703
inboundingbox <107548903+inboundingbox@users.noreply.github.com>
2022-11-30 09:45:07 +0100

    Add files via upload

---
+++ 02a - Settimana Somministrazione/word/src/main/java/it/cgmconsulting/word/WordApplication.java
@@ -0,0 +1,14 @@
+package it.cgmconsulting.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordApplication.class, args);
+	}
+
+}
