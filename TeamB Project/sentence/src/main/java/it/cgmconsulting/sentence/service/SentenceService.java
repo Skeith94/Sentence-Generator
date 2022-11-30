da70874fb4cd4009f564ab1baba876b609bc0dac
inboundingbox <107548903+inboundingbox@users.noreply.github.com>
2022-11-30 10:04:45 +0100

    Update SentenceService.java

--- TeamB Project/sentence/src/main/java/it/cgmconsulting/sentence/service/SentenceService.java
+++ TeamB Project/sentence/src/main/java/it/cgmconsulting/sentence/service/SentenceService.java
@@ -16,12 +16,7 @@ public class SentenceService {
     @Autowired ObjectService objectService;
 
     public  Mono<ResponseEntity<?>> getSentence() {
-        /*Flux<String> mergedMonos = subjectService.getWord().map(s -> s.concat(" "))
-                                    .concatWith(verbService.getWord().map(s -> s.concat(" "))
-                                    .concatWith(objectService.getWord().map(s -> s.concat("."))));
-
-        return mergedMonos.reduce(String::concat);*/
-
+        
         Mono<Tuple3<String, String, String>> zipped = Mono.zip(subjectService.getWord(), verbService.getWord(), objectService.getWord());
         return zipped.map(t -> ResponseEntity.status(HttpStatus.OK).body(t.getT1() + " " + t.getT2() + " " + t.getT3() + "."));
 
